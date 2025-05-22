fe1d8b1c25ddb65711e8e61120e8e0d56b835ff2
SameerK1234 <145307730+SameerK1234@users.noreply.github.com>
2025-05-22 15:13:26 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -14,8 +14,8 @@ def get_or_build_tokenizer(config,ds,lang):
   tokenizer_path = Path(config["tokenizer_file"].format(lang))
   if not Path.exists(tokenizer_path):
     tokenizer = Tokenizer(WordLevel(unk_tokens="[UNK]"))
-    tokenizer.pre_tokenizer = WhiteSpace()
-    trainer = WordLevelTrainer(special_token=["[UNK]","[EOS]","[SOS]","[PAD]")
+    tokenizer.pre_tokenizer = Whitespace()
+    trainer = WordLevelTrainer(special_tokens=["[UNK]","[EOS]","[SOS]","[PAD]")
     tokenizer.train_from_iterator(get_all_sentences(ds,lang),trainer=trainer)
     tokenizer.save(str(tokenizer_path))
   else:
