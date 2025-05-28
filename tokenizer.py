ab91cac01d3696819deb6b60368acce619d4e3ae
Sameer Khan <145307730+SameerK1234@users.noreply.github.com>
2025-05-28 17:38:49 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 from tokenizers import Tokenizer
-from tokenizers.model import WordLevel
+from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
-from tokenizers.pre_tokenizers import WhiteSpace
-from joblib import Path
+from tokenizers.pre_tokenizers import Whitespace
+from pathlib import Path
 from datasets import load_dataset
 from torch.utils.data import random_split,Dataset,DataLoader
 from dataset import BilingualDataset
@@ -15,21 +15,21 @@ def get_all_sentences(ds,lang):
 def get_or_build_tokenizer(config,ds,lang):
   tokenizer_path = Path(config["tokenizer_file"].format(lang))
   if not Path.exists(tokenizer_path):
-    tokenizer = Tokenizer(WordLevel(unk_tokens="[UNK]"))
+    tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
     tokenizer.pre_tokenizer = Whitespace()
-    trainer = WordLevelTrainer(special_tokens=["[UNK]","[EOS]","[SOS]","[PAD]")
+    trainer = WordLevelTrainer(special_tokens=["[UNK]","[EOS]","[SOS]","[PAD]"])
     tokenizer.train_from_iterator(get_all_sentences(ds,lang),trainer=trainer)
     tokenizer.save(str(tokenizer_path))
   else:
     tokenizer = Tokenizer.from_file(str(tokenizer_path))
   return tokenizer
 def get_ds(config):
-  ds_raw = load_dataset("opus_books",f'{config[lang_src]}-{config[lang_tgt]}',split="train")
+  ds_raw = load_dataset("opus_books",f'{config["lang_src"]}-{config["lang_tgt"]}',split="train")
 
-  tokenizer_src = get_or_build_tokenizer(confid,ds,config[lang_src])
-  tokenizer_tgt = get_or_build_tokenizer(confid,ds,config[lang_tgt])
+  tokenizer_src = get_or_build_tokenizer(confiG,ds,config[lang_src])
+  tokenizer_tgt = get_or_build_tokenizer(config,ds,config[lang_tgt])
 
-  train_ds_size = int(0.9*len(ds_raw)
+  train_ds_size = int(0.9*len(ds_raw))
   valid_ds_size = len(ds_raw) - train_ds_size
   train_ds,valid_ds = random_split(ds_raw,[train_ds_size,valid_ds_size])
 
